ff07081603c29ea32d2c29e4840e1cc98c824bf4
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-10-30 22:04:26 -0400

    sum of input with user input completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        int sum = 0;
+        int i = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = scan.nextInt();
+            for (i = 0; i <= n; i++){
+                sum += i;
+            }
+        System.out.println(sum);
+    }
 
     }
-    
-    int sumOfNumbers(int n) {
-        return 0;
-    }
-    
+
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
-    }
+    //int gaussianSumOfNumbers(int n)
+        //return 0;
    
     // for Extra extra credit - compare the methods and show which one is faster
-    
-}
+
